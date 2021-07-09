90e8a1fb96e351a9bbe573bf7f691c3d101f7cac
dmuszynski <d.muszynski112@gmail.com>
2021-07-09 19:47:46 +0200

    Add AbstaractEntity model class

---
+++ src/main/java/pl/dmuszynski/reviewit/model/AbstractEntity.java
@@ -0,0 +1,39 @@
+package pl.dmuszynski.reviewit.model;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    protected abstract static class Builder<T extends Builder<T>> {
+        private long id;
+
+        public T id(long id) {
+            this.id = id;
+            return this.self();
+        }
+
+        public abstract AbstractEntity build();
+        protected abstract T self();
+    }
+
+    protected AbstractEntity() { }
+
+    protected AbstractEntity(long id) {
+        this.id = id;
+    }
+
+    protected AbstractEntity(Builder<?> builder) {
+        this.id = builder.id;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+}
