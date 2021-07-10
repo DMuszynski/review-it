c6d29dd4cb1e06f5d0d3233bdd7075c32b3c2e23
dmuszynski <d.muszynski112@gmail.com>
2021-07-10 22:45:34 +0200

    Add ResourceConfig class

--- src/main/java/pl/dmuszynski/reviewit/service/user/impl/RegisterServiceImpl.java
+++ src/main/java/pl/dmuszynski/reviewit/service/user/impl/RegisterServiceImpl.java
@@ -1,23 +1,37 @@
 package pl.dmuszynski.reviewit.service.user.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.dmuszynski.reviewit.dto.request.SignUpRequestDto;
+import pl.dmuszynski.reviewit.model.user.Authority;
+import pl.dmuszynski.reviewit.model.user.AuthorityType;
 import pl.dmuszynski.reviewit.model.user.User;
+import pl.dmuszynski.reviewit.repository.user.AuthorityRepository;
 import pl.dmuszynski.reviewit.repository.user.UserRepository;
 import pl.dmuszynski.reviewit.service.user.RegisterService;
+
+import java.util.Collections;
+import java.util.Set;
 
 @Service(value = "registerService")
 public class RegisterServiceImpl implements RegisterService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthorityRepository authorityRepository;
 
     @Autowired
-    public RegisterServiceImpl(UserRepository userRepository) {
+    public RegisterServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder,
+                               AuthorityRepository authorityRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authorityRepository = authorityRepository;
     }
 
     @Override
     public void signUp(SignUpRequestDto signUpDetails) {
-
+        final String encodedPassword = this.passwordEncoder.encode(signUpDetails.getPassword());
+        //final Set<Authority> authorities = Collections.singleton(this.authorityRepository.findByAuthorityType(AuthorityType.USER).orE);
+        //this.userRepository.save(new User.Builder(signUpDetails.getEmail(), signUpDetails.getUsername(), encodedPassword))
     }
 }
