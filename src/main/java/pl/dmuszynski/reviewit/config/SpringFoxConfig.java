f90d7a96cce585089b1bcb639871f9fdfe68aec0
dmuszynski <d.muszynski112@gmail.com>
2021-07-13 00:17:02 +0200

    Add SpringFox Configuration

---
+++ src/main/java/pl/dmuszynski/reviewit/config/SpringFoxConfig.java
@@ -0,0 +1,34 @@
+package pl.dmuszynski.reviewit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket getApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .paths(PathSelectors.ant("/review-it/**"))
+                .apis(RequestHandlerSelectors.basePackage("pl.dmuszynski.reviewit.api"))
+                .build().apiInfo(this.getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Review-it API")
+                .description("Movie discovery and review - Web application")
+                .contact(new Contact("Damian Muszyński", " ", "d.muszynski112@gmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .version("0.0.1-SNAPSHOT")
+                .build();
+    }
+}
