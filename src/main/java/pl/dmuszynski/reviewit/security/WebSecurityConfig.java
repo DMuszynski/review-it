28110688f6a8d4084406dd9d5bf686eda08fba72
dmuszynski <d.muszynski112@gmail.com>
2021-07-12 13:21:56 +0200

    Configure springfox swagger library

---
+++ src/main/java/pl/dmuszynski/reviewit/security/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package pl.dmuszynski.reviewit.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/swagger-resources/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                .antMatchers("/review-it/account/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+}
